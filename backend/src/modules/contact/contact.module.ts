40e017726cba2bf26940ca1518023e33dcd861be
Thai <hoangthai229@gmail.com>
2026-03-23 23:10:44 +0700

    add Turnstile

--- backend/src/modules/contact/contact.module.ts
+++ backend/src/modules/contact/contact.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
 import { NotificationModule } from '../notification/notification.module';
+import { TurnstileModule } from '../../common/turnstile/turnstile.module';
+import { TurnstileService } from '../../common/turnstile/turnstile.service';
 
 @Module({
-  imports: [NotificationModule],
+  imports: [TurnstileModule, NotificationModule],
   controllers: [ContactController],
-  providers: [ContactService],
+  providers: [ContactService, TurnstileService],
 })
 export class ContactModule {}
